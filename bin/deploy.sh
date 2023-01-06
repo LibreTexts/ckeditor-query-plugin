f33cf57879cee18d55d52151083ae2f64b7d97f4
Tim <timstewartj@gmail.com>
2023-01-06 10:28:10 -0800

    add branch and hash to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 HOME_DIR=/home/jupyterteam
-BRANCH=staging
+BRANCH=$(git branch --show-current)
+ESCAPED_BRANCH=$(printf '%s\n' "$BRANCH" | sed -e 's/[\/&]/\\&/g')
+HASH=$(git rev-parse --short HEAD)
 
 if [ $# -eq 1 ]
 then
@@ -10,6 +12,7 @@ ssh -tt jupyterteam@test.libretexts.org << EOF
 cd $HOME_DIR/ckeditor-query-plugin
 git fetch --all
 git checkout $BRANCH && git reset --hard origin/$BRANCH
+sed -i "s/Development Version/Development Version - $ESCAPED_BRANCH - $HASH/" src/scripts/plugin.js
 yarn install
 yarn build
 cp dist/queryPlugin.min.js $HOME_DIR/public
